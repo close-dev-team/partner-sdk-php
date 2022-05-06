7cf7edd9af5a9fb759bedd6bc8ceee36a562765f
David Close <david@thecloseapp.com>
2022-05-06 09:07:48 +0200

    Define client resolver

---
+++ tests/ClientBuilderTest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace ClosePartnerSdk\Tests;
+
+use ClosePartnerSdk\ClientBuilder;
+use Http\Client\Common\Plugin\HeaderDefaultsPlugin;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientInterface;
+
+class ClientBuilderTest extends TestCase
+{
+    /** @test **/
+    public function resolve_http_client_that_implements_psr()
+    {
+        $client = (new ClientBuilder)->getHttpClient();
+        self::assertInstanceOf(ClientInterface::class, $client);
+    }
+
+    /** @test **/
+    public function be_able_to_add_plugins_into_the_client()
+    {
+        $clientBuilder = new ClientBuilder;
+        $clientBuilder->addPlugin(new HeaderDefaultsPlugin([
+            'Accept' => 'application/json',
+        ]));
+
+        $client = (new ClientBuilder)->getHttpClient();
+        self::assertInstanceOf(ClientInterface::class, $client);
+    }
+}
