75cc90adac60f3074f0f1b564bdf7a1bc5434ed7
dpenas <dariosoyyo@gmail.com>
2022-05-16 16:28:51 +0200

    CL-7196 - Added the rest of the functions to send messages

--- src/Endpoint/SendMessage.php
+++ src/Endpoint/SendMessage.php
@@ -3,8 +3,10 @@ declare(strict_types=1);
 
 namespace ClosePartnerSdk\Endpoint;
 
+use ClosePartnerSdk\Dto\ChatId;
 use ClosePartnerSdk\Dto\EventId;
 use ClosePartnerSdk\Dto\Mapper\SendMessageMapper;
+use ClosePartnerSdk\Dto\UserId;
 use ClosePartnerSdk\HttpClient\Message\RequestBodyMediator;
 
 final class SendMessage extends CloseEndpoint
@@ -15,6 +17,48 @@ final class SendMessage extends CloseEndpoint
             ->getHttpClient()
             ->post(
                 $this->buildUriWithLatestVersion('/events/'.$eventId.'/messages/text'),
+                [],
+                RequestBodyMediator::convertStreamFromArray(
+                    $this->sdk,
+                    SendMessageMapper::withText($text)
+                )
+            );
+    }
+
+    public function toAllUsersForChat(EventId $eventId, ChatId $chatId, string $text): void
+    {
+        $this->sdk
+            ->getHttpClient()
+            ->post(
+                $this->buildUriWithLatestVersion('/events/'.$eventId.'chats/'.$chatId.'/messages/text'),
+                [],
+                RequestBodyMediator::convertStreamFromArray(
+                    $this->sdk,
+                    SendMessageMapper::withText($text)
+                )
+            );
+    }
+
+    public function toUserInChat(EventId $eventId, ChatId $chatId, UserId $userId, string $text): void
+    {
+        $this->sdk
+            ->getHttpClient()
+            ->post(
+                $this->buildUriWithLatestVersion('/events/'.$eventId.'chats/'.$chatId.'users/'.$userId.'/messages/text'),
+                [],
+                RequestBodyMediator::convertStreamFromArray(
+                    $this->sdk,
+                    SendMessageMapper::withText($text)
+                )
+            );
+    }
+
+    public function toUserInAllChats(EventId $eventId, UserId $userId, string $text): void
+    {
+        $this->sdk
+            ->getHttpClient()
+            ->post(
+                $this->buildUriWithLatestVersion('/events/'.$eventId.'users/'.$userId.'/messages/text'),
                 [],
                 RequestBodyMediator::convertStreamFromArray(
                     $this->sdk,
