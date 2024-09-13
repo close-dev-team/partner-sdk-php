61c642030f1f05b2c65d29cd5cf14672c951c267
ww <admin@localhost>
2024-09-13 10:30:02 +0200

    Added two tests for this new base_uri feature

--- tests/OptionsTest.php
+++ tests/OptionsTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace ClosePartnerSdk\Tests;
 
 use ClosePartnerSdk\Options;
+use ClosePartnerSdk\Config;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\OptionsResolver\Exception\MissingOptionsException;
 
@@ -53,5 +54,30 @@ class OptionsTest extends TestCase
         $authCredentials = $options->getAuthCredentials();
         self::assertEquals($clientId, $authCredentials->getClientId());
         self::assertEquals($clientSecret, $authCredentials->getClientSecret());
+    }
+
+    /** @test **/
+    public function provide_default_base_uri_for_production_endpoint(): void
+    {
+        $options = new Options([
+            'client_id' => '1234',
+            'client_secret' => 'secret',
+        ]);
+
+        self::assertNotEmpty($options->getBaseUri());
+        self::assertEquals(Config::BASE_URI, $options->getBaseUri());
+    }
+
+    /** @test **/
+    public function provide_default_custom_endpoint_for_testing_or_sdk_platforms(): void
+    {
+        $options = new Options([
+            'client_id' => '1234',
+            'client_secret' => 'secret',
+            'base_uri' => 'http://localhost:8080/api',
+        ]);
+
+        self::assertNotEmpty($options->getBaseUri());
+        self::assertEquals('http://localhost:8080/api', $options->getBaseUri());
     }
 }
